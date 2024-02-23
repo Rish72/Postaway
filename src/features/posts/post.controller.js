1a825e9f42d034423cd7993941e20842fad0d7dc
Rishabh <rishabhgoel2020@gmail.com>
2024-02-23 18:52:02 +0530

    Add all folders and controller, models and routes file

---
+++ src/features/posts/post.controller.js
@@ -0,0 +1,28 @@
+import PostModel from "./post.model.js";
+
+export default class PostController{
+
+    getAll(req, res){
+
+    }
+
+    getOne(req, res){
+
+    }
+
+    getPostOfUser(req, res){    // Get post using User Credentials
+
+    }
+
+    addPost(req, res){
+
+    }
+
+    deletePost(req, res){
+
+    }
+
+    updatePost(req, res){
+
+    }
+}
---
+++ src/features/posts/post.model.js
@@ -0,0 +1,32 @@
+
+export default class PostModel{
+    constructor(){
+
+    }
+
+    static getPosts(){
+
+    }
+
+    static getOne(){
+
+    }
+
+    static getUserPost(){       // give posts based on User Credentials
+
+    }
+
+    static add(){
+
+    }
+
+    static update(){
+
+    }
+
+    static delete(){
+
+    }
+}
+
+let posts = [{},{}];
