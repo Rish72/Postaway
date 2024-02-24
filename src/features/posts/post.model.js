f97b8d80a9dda422425d0d1a74a229d8c58ad26d
Rishabh <rishabhgoel2020@gmail.com>
2024-02-24 22:34:36 +0530

    Implement : Post Model(few features are left), Post routes

--- src/features/posts/post.model.js
+++ src/features/posts/post.model.js
@@ -1,23 +1,33 @@
 
 export default class PostModel{
-    constructor(){
-
+    constructor(id , userID, caption, imageUrl){
+        this.id = id;
+        this.userID = userID;
+        this.caption = caption;
+        this.imageUrl = imageUrl;
     }
 
     static getPosts(){
-
+        return posts;
     }
 
-    static getOne(){
-
+    static getOne(id){
+        const post = post.find( post => post.id === id);
+        console.log(`Specific POST of ${id} : ` +post);
+        return post
     }
 
-    static getUserPost(){       // give posts based on User Credentials
-
+    static getUserPost(data){       // give posts based on User Credentials
+        const {userID} = data;
+        const UserSpecificPosts = posts.filter( post => post.userID === userID);
+        return UserSpecificPosts;
     }
 
     static add(){
-
+        const {userID, caption , imageUrl} = data
+        const newPost = new PostModel(post.length+1, userID, caption, imageUrl);
+        posts.push(newPost);
+        return newPost
     }
 
     static update(){
@@ -29,4 +39,17 @@ export default class PostModel{
     }
 }
 
-let posts = [{},{}];
+let posts = [
+    {
+        id : 1, 
+        userID : 1,
+        caption: "New Post about movies",
+        imageUrl : "https://hips.hearstapps.com/hmg-prod/images/best-fall-movies-1659459329.jpg?crop=0.937xw:1.00xh;0.0327xw,0&resize=1200:*"
+    },
+    {
+        id : 2, 
+        userID : 1,
+        caption: "New Post about Cars",
+        imageUrl : "https://static.autox.com/uploads/2022/11/2023-BMW-S1000RR.jpg"
+    }
+];
--- src/features/posts/post.routes.js
+++ src/features/posts/post.routes.js
@@ -1,3 +1,13 @@
+import PostController from "./post.controller.js";
 import express from "express";
 
+const postController = new PostController();
 const postRouter = express.Router();
+
+postRouter.get('/', postController.getAll)
+postRouter.get('/:id', postController.getOne)
+// postRouter.get('/:id', postController.getOne)
+
+postRouter.post("/addPost", postController.addPost);
+postRouter.delete("/delete/:id", postController.deletePost);
+postRouter.put("/updat/:id", postController.updatePost);
