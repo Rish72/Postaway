be32b75202b7e21a44c46f3458eca230be60d48d
Rishabh <rishabhgoel2020@gmail.com>
2024-02-27 12:01:40 +0530

    Implement : Add Routes for the Comments

--- src/features/comments/comm.routes.js
+++ src/features/comments/comm.routes.js
@@ -1,6 +1,11 @@
 import express from "express";
-
+import CommentsController from "./comm.controller.js";
 const commentsRouter = express.Router();
 
+const commentsController = new CommentsController();
 
 
+commentsRouter.get('/:id', commentsController.getAllComments);
+commentsRouter.post('/addComment', commentsController.addComment);
+
+export default commentsRouter;
